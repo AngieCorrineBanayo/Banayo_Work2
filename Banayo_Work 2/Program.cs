60f274a29312717ff38d3b028a57dadaf53fffb6
Angie Banayo <abanayo@csmbataan.ph.education>
2024-03-03 19:40:41 -1200

    Banayo_Output 2

---
+++ Banayo_Work 2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banayo_Work_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i = 0;
+            Console.WriteLine("Enter The Number");
+            i = int.Parse(Console.ReadLine());
+            if (i % 15== 3)
+            {
+                Console.WriteLine(i + " Is a Even Number");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine(i + " Is a Odd Number");
+                Console.ReadLine();
+            }
+        }
+    }
+}
+    
+
